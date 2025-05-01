d39c8a1deb3c06a4ca23ade4470487613d5c168d
Suliman Benhalim <benhalim@unhcr.org>
2025-05-01 13:39:54 +0200

    Implement service provider with configuration publishing

--- config/zyna.php
+++ config/zyna.php
@@ -1,5 +1,59 @@
 <?php
 
 return [
-    // Package configuration will be added in v0.0.2
+    /*
+    |--------------------------------------------------------------------------
+    | Component Implementation
+    |--------------------------------------------------------------------------
+    |
+    | This value determines which implementation of the components will be used.
+    | Options: 'blade' or 'livewire'
+    |
+    */
+    'components' => [
+        'implementation' => 'blade', // Available options: 'blade', 'livewire'
+        'prefix' => 'zyna', // The prefix used for component registration
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Theme Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Configure the default theme settings for all components.
+    | These values can be overridden by the user.
+    |
+    */
+    'themes' => [
+        // Theme configuration will be expanded in v0.0.4
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | JavaScript Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Configure the JavaScript integration settings.
+    |
+    */
+    'javascript' => [
+        'publicPath' => 'vendor/zyna',
+        'autoload' => true,
+        'deferLoading' => false,
+        'useCdn' => false,
+        'cdnVersion' => '3.0.0',
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Icon Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Configure the default icon provider.
+    |
+    */
+    'icons' => [
+        'provider' => 'heroicons',
+        'defaultSet' => 'outline',
+    ],
 ];
--- src/ZynaServiceProvider.php
+++ src/ZynaServiceProvider.php
@@ -13,7 +13,10 @@ class ZynaServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        // Register package services
+        // Merge package configuration
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/zyna.php', 'zyna'
+        );
     }
 
     /**
@@ -23,6 +26,30 @@ class ZynaServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // Bootstrap package services
+        // Publish configuration
+        $this->publishes([
+            __DIR__.'/../config/zyna.php' => config_path('zyna.php'),
+        ], 'zyna-config');
+
+        // Publish assets
+        $this->publishes([
+            __DIR__.'/../dist' => public_path('vendor/zyna'),
+        ], 'zyna-assets');
+
+        // Publish views
+        $this->publishes([
+            __DIR__.'/../resources/views' => resource_path('views/vendor/zyna'),
+        ], 'zyna-views');
+
+        // Load views
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'zyna');
+
+        // Check component implementation setting
+        // This logic will be expanded in v0.0.6 when implementing the component registration system
+        $implementation = config('zyna.components.implementation', 'blade');
+        if ($implementation === 'livewire' && !class_exists(\Livewire\Livewire::class)) {
+            // Fallback to blade if Livewire is selected but not installed
+            config(['zyna.components.implementation' => 'blade']);
+        }
     }
 }
